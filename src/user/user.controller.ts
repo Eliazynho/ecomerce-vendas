347892079877732a32c13daf78618df575c2bafc
eliazynho <m10elias10@gmail.com>
2025-03-24 21:55:33 -0300

    criando rotas user

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,13 +1,17 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Body, Get } from '@nestjs/common';
 import { CreateUserDto } from './dtos/createUser.dto';
+import { UserService } from './user.service';
 
 @Controller('user')
 export class UserController {
+  constructor(private readonly userService: UserService) {}
   @Post()
   createUser(@Body() createUser: CreateUserDto) {
-    return {
-      ...createUser,
-      password: undefined,
-    };
+    return this.userService.createUser(createUser);
+  }
+
+  @Get()
+  getAllUsers() {
+    return this.userService.getAllUsers();
   }
 }
